5ca329bf813a130e75d57b20867f8dd1233c83da
nicohnavarro <nicohnavarro@gmail.com>
2021-09-02 01:57:42 -0300

    :hammer: Folder Structure Client

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import express from "express";
 import "./config/mongoose";
 import productRoutes from "./routes/products.routes";
+import {PORT} from './config';
 
 const app = express();
 app.use(express.json());
 app.use(productRoutes);
-app.listen(3000);
-console.log("Server on port ", 3000);
+app.listen(PORT);
+console.log(`Server on port ${PORT}`);
