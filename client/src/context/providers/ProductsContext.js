7922b92cfdd0f2226dd31ff37859eee3e20601b7
nicohnavarro <nicohnavarro@gmail.com>
2021-09-09 23:30:41 -0300

    :hammer: add Actions

--- client/src/context/providers/ProductsContext.js
+++ client/src/context/providers/ProductsContext.js
@@ -1,30 +1,38 @@
-import { createContext, useEffect, useReducer } from "react";
+import { createContext, useContext, useEffect, useReducer } from "react";
 import { getProducts } from "../../api/productApi";
 import { productsReducer, initialState } from "../reducers/productsReducer";
 import { productActions } from "../actions/productsActions";
+
 export const ProductContext = createContext(initialState);
+
+export const useProducts = () => {
+  const context = useContext(ProductContext);
+  return context;
+};
 
 export const ProductProvider = ({ children }) => {
   const [state, dispatch] = useReducer(productsReducer, initialState);
 
   const loadProducts = async () => {
     dispatch({ type: productActions.LOAD_PRODUCTS });
-    const res = await getProducts();
-    if (res.data)
-      dispatch({
-        type: productActions.LOAD_PRODUCTS_SUCCESS,
-        payload: res.data,
-      });
-    else
+    try {
+      const res = await getProducts();
+      if (res.data)
+        dispatch({
+          type: productActions.LOAD_PRODUCTS_SUCCESS,
+          payload: res.data,
+        });
+    } catch (err) {
       dispatch({
         type: productActions.LOAD_PRODUCTS_ERROR,
-        payload: "No products",
+        payload: err.message,
       });
+    }
   };
 
   useEffect(() => {
     loadProducts();
-  }, []); 
+  }, []);
 
   return (
     <ProductContext.Provider value={{ ...state }}>
